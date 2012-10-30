e48c7436163cb6ec4dd9794f94ace5f47a01ab70
Max Lv <max.c.lv@gmail.com>
2012-10-30 10:19:42 +0800

    init

---
+++ assets/proxy_socks.sh
@@ -0,0 +1,49 @@
+#!/system/bin/sh
+
+DIR=/data/data/org.sshtunnel.beta
+
+PATH=$DIR:$PATH
+
+case $1 in
+ start)
+
+echo "
+base {
+ log_debug = off;
+ log_info = off;
+ log = stderr;
+ daemon = on;
+ redirector = iptables;
+}
+" >$DIR/redsocks.conf
+
+   echo "
+redsocks {
+ local_ip = 127.0.0.1;
+ local_port = 8123;
+ ip = 127.0.0.1;
+ port = $2;
+ type = socks5;
+} 
+" >>$DIR/redsocks.conf
+
+  $DIR/redsocks -p $DIR/redsocks.pid -c $DIR/redsocks.conf
+  
+  ;;
+stop)
+  kill -9 `cat $DIR/redsocks.pid`
+  
+  kill -9 `cat $DIR/ssh.pid`
+  
+  killall -9 redsocks
+  
+  killall -9 sshtunnel
+  
+  rm $DIR/redsocks.pid
+  
+  rm $DIR/redsocks.conf
+  
+  rm $DIR/ssh.pid
+  
+  ;;
+esac
